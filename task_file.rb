0458f369a943bdc8964af62f2ce3dae0e7b01e6b
kotsolesya <kots.olesya@gmail.com>
2016-05-25 10:53:54 +0300

    fix file task

--- task_file.rb
+++ task_file.rb
@@ -1,18 +1,44 @@
 class TaskFile
 
   def output
-   h= Hash[File.read('wordsEn.txt').split("\n").map{|i|i.split(' ')}]
+    ok_hash = {}
+    ok_mass = []
+    result={}
+    File.open("wordsEn.txt").readlines.each do |line|
+      # тут розпридфляєм по хешу де ключ це кількість симолів а значення - масив слів відповідної кількості
+      # для ключа нового створюєм масив [] а якщо є то просто дописуєм << line.strip
+      ok_hash.has_key?(line.length-2) ? ok_hash[line.length-2]<< line.strip : ok_hash[line.length-2] = []
+    end
 
-    h.each do |k,v|
-      if k=="#{k[0..-3]}#{k[-1]}#{k[-2]}"
-        puts "#{k} \t\t-- #{k[0..-3]}#{k[-1]}#{k[-2]}"
+    ok_hash.each do |k,v|
+      # тут відкидаєм слова до двох символів
+        if k>=0 && k<=2
+          next
+        end
+      # перевірка чи є слова з відповідні до задачі
+        v.each do |element|
+          if result.has_key?("#{element[0..-3]}#{element[-1]}#{element[-2]}")
+            if element!="#{element[0..-3]}#{element[-1]}#{element[-2]}"
+            result["#{element[0..-3]}#{element[-1]}#{element[-2]}"]=element
+          #  puts "#{element}\t\t#{element[0..-3]}#{element[-1]}#{element[-2]}"
+            end
+          else
+            result["#{element}"]=nil
+          end
+
+        end
+    end
+
+    result.each do |k,v|
+      if v!=nil
+        puts "[#{k},\t#{v}]"
       end
 
     end
   end
 
-
-end
 tf = TaskFile.new()
 tf.output
+end
+
 
